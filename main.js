08cdf76585abf66b84571bba4c8a65a911653ab0
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-03-24 12:03:10 +0100

    add h2

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ import "./sass/style.scss";
 
 document.querySelector("#app").innerHTML = `
   <h1>Hello Agata & Zuzana!</h1>
+  <h2>This is our first collaboration on Carbon calculator thingy!</h2>
+  
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
