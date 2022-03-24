8b3aa5ab9ad2c6dcdf8cc24af1027c43f1fd10ed
Agata Szulc <agataszulc97@gmail.com>
2022-03-24 12:06:25 +0100

    testing testing

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./sass/style.scss";
 document.querySelector("#app").innerHTML = `
   <h1>Hello Agata & Zuzana!</h1>
   <h2>This is our first collaboration on Carbon calculator thingy!</h2>
+  <h2>wow</h2>
   
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
