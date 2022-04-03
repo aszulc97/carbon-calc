0866a46de174feea04bc7e5f086799a0f9d1d611
Agata Szulc <agataszulc97@gmail.com>
2022-04-03 19:53:15 +0200

    fetched data from both APIs

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 import "./sass/style.scss";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Agata & Zuzana!</h1>
-  <h2>This is our first collaboration on Carbon calculator thingy!</h2>
-  
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const webCarbonUrl = "https://kea-alt-del.dk/websitecarbon/site/?url=";
+
+document.querySelector("button").addEventListener("click", getWebsite);
+
+let website;
+
+function getWebsite() {
+  website = document.querySelector("input").value;
+  fetchData();
+}
+
+function fetchData() {
+  fetch(webCarbonUrl + website)
+    .then((res) => res.json())
+    .then((data) => showWebcarbonData(data));
+}
+
+function showWebcarbonData(data) {
+  document.querySelector("h1").textContent = "Your website: " + website;
+  document.querySelector("#size").textContent = data.bytes + " bytes";
+  run();
+}
+
+function showGoogleData(data) {
+  console.log(data);
+  document.querySelector("#mini").textContent = data.lighthouseResult.audits["unminified-javascript"].description;
+}
+
+function run() {
+  const url = setUpQuery();
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showGoogleData(data));
+}
+
+function setUpQuery() {
+  const api = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed";
+  const parameters = {
+    url: website,
+    key: "AIzaSyCCsJR7PD4hGIi2x8r2y8B4rgpYooeICYQ",
+  };
+  let query = `${api}?url=${parameters.url}&key=${parameters.key}`;
+  return query;
+}
