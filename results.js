2914dd4aa2d1468d37d57e2620c7d318eee21c78
Agata Szulc <agataszulc97@gmail.com>
2022-04-07 10:41:02 +0200

    posting to db works

--- results.js
+++ results.js
@@ -1,21 +1,49 @@
-//import { APIKEY } from "./config.js";
+import { db_APIKEY } from "./config.js";
 import "./sass/style.scss";
 const urlParams = new URLSearchParams(window.location.search);
 const website = urlParams.get("url");
-const webCarbonUrl = "https://kea-alt-del.dk/websitecarbon/site/?url=";
+const industry = urlParams.get("industry");
+const webCarbonURL = "https://kea-alt-del.dk/websitecarbon/site/?url=";
+const dbURL = "https://serialkillers-7bdb.restdb.io/rest/carboncalc";
 
 fetchData();
 
 function fetchData() {
-  fetch(webCarbonUrl + website)
+  fetch(webCarbonURL + website)
     .then((res) => res.json())
     .then((data) => showWebcarbonData(data));
 }
 
 function showWebcarbonData(data) {
+  post(data);
+  console.log(data);
   document.querySelector(".website").textContent = "Your website: " + website;
-  document.querySelector(".bytes").textContent = data.bytes + " bytes";
+  document.querySelector(".bytes").textContent = data.statistics.co2.grid.grams;
   // run();
+}
+
+function post(data) {
+  const newURL = {
+    URL: website,
+    totalbytes: data.bytes,
+    co2: data.statistics.co2.grid.grams,
+    greenhost: data.green,
+    industry: industry,
+  };
+  if (data.green === "unknown") {
+    newURL.greenhost = false;
+  }
+  const postData = JSON.stringify(newURL);
+  fetch(dbURL, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      "x-apikey": db_APIKEY,
+    },
+    body: postData,
+  }).then((res) => res.json());
+  //.then((data) => displayCar(data));
+  console.log("posted");
 }
 
 // function showGoogleData(data) {
